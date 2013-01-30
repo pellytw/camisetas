08d74d5a1499e609683942cba0664e96d8cba951
Pelly <marcelo.eduardo.gonzalez@gmail.com>
2013-01-30 13:05:55 -0300

    mejora en imagenes

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -10,6 +10,14 @@ class Producto < ActiveRecord::Base
   belongs_to :talle
   belongs_to :titularidad
   
+  validates :club_id, :presence => true
+  validates :tipo_producto_id, :presence => true
+  validates :precio_publico, :presence => true
+  validates :talle_id, :presence => true
+  validates :tipo_producto_id, :presence => true
+  validates :titularidad, :presence => true
+  validates :stock, :presence => true
+  
   def to_s
     self.club.to_s + " " +  self.titularidad.to_s + " " + self.talle.to_s 
   end  
